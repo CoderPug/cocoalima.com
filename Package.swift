c4b15249a3298a1d07d6218818025cbe7319f346
Jose Torres <torrescardenasjose@gmail.com>
2017-01-15 13:01:13 -0500

    Initialize empty Vapor project.

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "cocoalima",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+    ]
+)
+
