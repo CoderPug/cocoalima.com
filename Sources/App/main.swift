ce78c517138d505a2312e537966956bbcc5834ef
Jose Torres <torrescardenasjose@gmail.com>
2017-01-15 13:17:47 -0500

    Implemented first draft for the welcome page as a 'work in progress'.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -4,8 +4,10 @@ let drop = Droplet()
 
 drop.get { req in
     return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+        "message": drop.localization[req.lang, "welcome", "message"],
+        "announcement-mainswift": drop.localization[req.lang, "announcements", "mainswift"],
+        "announcement-twitter": drop.localization[req.lang, "announcements", "twitter"],
+        ])
 }
 
 drop.run()
